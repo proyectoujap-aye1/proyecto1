5c2c69e9da62df3cb33cb280083f7081686fb927
Juan Gualdron <juanmgualdronm@gmail.com>
2025-06-24 16:03:24 -0400

    inicializacion de matrices

--- src/Process.java
+++ src/Process.java
@@ -6,7 +6,70 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Process {
+ public static void initMatrix (boolean[] m) {
+        if (m != null) {
+            for (int i = 0; i < m.length; i++)
+                m[i] = false;
+        }
+    }
 
+    public static void initMatrix (int[] m) {
+        if (m != null) {
+            for (int i = 0; i < m.length; i++)
+                m[i] = 0;
+        }
+    }
+
+    public static void initMatrix (String[][] m) {
+        if (m != null) {
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[0].length; j++)
+                    m[i][j] = "";
+            }
+        }
+    }
+
+    public static void initMatrix (int[][] m) {
+        if (m != null) {
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[0].length; j++)
+                    m[i][j] = 0;
+            }
+        }
+    }
+
+    public static void initMatrix (String[][][] m) {
+        if (m != null) {
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[0].length; j++) {
+                    for (int k = 0; k < m[0][0].length; k++)
+                        m[i][j][k] = "";
+                }
+            }
+        }
+    }
+
+    public static void initMatrix (int[][][] m) {
+        if (m != null) {
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[0].length; j++) {
+                    for (int k = 0; k < m[0][0].length; k++)
+                        m[i][j][k] = 0;
+                }
+            }
+        }
+    }
+
+    public static void initMatrix (double[][][] m) {
+        if (m != null) {
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[0].length; j++) {
+                    for (int k = 0; k < m[0][0].length; k++)
+                        m[i][j][k] = 0;
+                }
+            }
+        }
+    }
     public static void requestNames (Scanner scanner, int tableNumber, int personsNumber, String[][] namePersons) {
         for (int i = 0; i < personsNumber; i++)
             namePersons[tableNumber - 1][i] = Validate.scanValidString(scanner, "Nombre de persona " + (i + 1) + ": ");
@@ -83,3 +146,4 @@ public class Process {
         }
     }
 }
+
